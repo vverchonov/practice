db964a6000208075490d81329009e7b6be8a488d
vverchonov <vverchonov@gmail.com>
2022-08-04 14:03:51 -0700

    Django routes
    - routes
    - serializers
    - models

---
+++ ppractice/myDjangoApp/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
--- ppractice/ppractice/settings.py
+++ ppractice/ppractice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myDjangoApp.apps.MydjangoappConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
