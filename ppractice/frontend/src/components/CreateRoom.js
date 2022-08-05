e3e22b24223d45c059ebc8b0b486023d2c049923
vverchonov <vverchonov@gmail.com>
2022-08-05 14:24:54 -0700

    UI
    - Basic UI with mui

--- ppractice/frontend/src/components/CreateRoom.js
+++ ppractice/frontend/src/components/CreateRoom.js
@@ -1,13 +1,77 @@
 import React, { Component } from "react";
 
+import Grid from '@mui/material/Grid';
+import { FormHelperText, TextField, Typography } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { RadioGroup } from "@mui/material";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from "@mui/material/Radio";
+import { Button } from "@mui/material";
+import { Link } from "@mui/material";
 
 
 export default class CreateRoom extends Component {
+    default_votes = 2
+
     constructor(props) {
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.default_votes
+        }
+
+
+        //this.handleCreateRoom = this.handleCreateRoom.bind(this);
+    }
+
+    handleVotesChanged = (e) => {
+        this.setState({ votesToSkip: e.target.value })
+    }
+
+    handleGuestCanPauseChange = (e) => {
+        this.setState({ guestCanPause: e.target.value === "true" ? true : false })
+    }
+
+    handleCreateRoom = () => {
+        console.log(this.state)
     }
 
     render() {
-        return <p>room create</p>
+        return (
+            <div>
+                <Grid container spacing={1}>
+                    <Grid item xs={12} align="center">
+                        <Typography component="h4" variant="h4">Create a Room</Typography>
+                    </Grid >
+                    <Grid item xs={12} align="center">
+                        <FormControl component="fieldset">
+                            <FormHelperText>
+                                <div align="center">
+                                    Guest Control of playback state
+                                </div>
+                            </FormHelperText>
+                            <RadioGroup row defaultValue={true} onChange={this.handleGuestCanPauseChange}>
+                                <FormControlLabel value={true} control={<Radio color="primary" />} label="play & pause" labelPlacement="bottom"></FormControlLabel>
+                                <FormControlLabel value={false} control={<Radio color="secondary" />} label="no control" labelPlacement="bottom"></FormControlLabel>
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid >
+                    <Grid item xs={12} align="center">
+                        <FormControl>
+                            <TextField required={true} type="number" onChange={this.handleVotesChanged} defaultValue={this.default_votes} inputProps={{ min: 1, style: { textAlign: "center" }, }} />
+                            <FormHelperText>
+                                <div align="center">
+                                    Votes required to skip the song
+                                </div>
+                            </FormHelperText>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12} align="center">
+                        <Button color="secondary" variant="contained" onClick={this.handleCreateRoom}>Create a room</Button>
+                        <Button color="primary" variant="contained" to="/" component={Link}>Back</Button>
+                    </Grid>
+                </Grid>
+            </div>
+        );
     }
 }
